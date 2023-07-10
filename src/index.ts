63bb926ca33f091af16dbd602507e06853865e7b
Timeuh <timeuuh@gmail.com>
2023-07-10 16:14:06 +0200

    feat: project base

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+// get express
+const express = require('express');
+// specify port for server
+const port = 5000;
+// init app
+const app = express();
+// init prisma
+const prisma = require('@prisma/client')
+
+// middleware to treat request data
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// run server with provided port
+app.listen(port, () => {
+  console.log(`Server up on port ${port}`);
+});
