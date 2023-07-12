ecb1493fbfe78642cb719fb8e3c5ab7a09cef8a6
Timeuh <timeuuh@gmail.com>
2023-07-12 21:10:42 +0200

    feat: register users routes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-// get express
 const express = require('express');
-// specify port for server
-const port = 5000;
+const dotenv = require('dotenv');
+
+// init dotenv config
+dotenv.config();
 // init app
 const app = express();
+// specify port for server
+const port = 5000;
 
 // middleware to treat request data
 app.use(express.json());
@@ -11,6 +14,9 @@ app.use(express.urlencoded({ extended: false }));
 
 // call routes to treat cards related requests
 app.use('/artists', require('./routes/artists.routes'));
+
+// call routes to treat user related requests
+app.use('/users', require('./routes/users.routes'));
 
 // run server with provided port
 app.listen(port, () => {
