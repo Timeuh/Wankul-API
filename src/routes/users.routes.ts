0032d62ad094df8affde66bb71f15d534523a9d9
Timeuh <timeuuh@gmail.com>
2023-07-13 16:42:50 +0200

    feat: register user update route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,6 +1,6 @@
 export {};
 
-const {createUser, getUser} = require('../controllers/user.controller');
+const {createUser, getUser, updateUser} = require('../controllers/user.controller');
 const router = require('express').Router();
 
 // route to create a user
@@ -8,6 +8,9 @@ router.post('/new', createUser);
 
 // route to get a user from an email
 router.get('/get', getUser);
+
+// route to update a user
+router.put('/update', updateUser);
 
 // export the router
 module.exports = router;
