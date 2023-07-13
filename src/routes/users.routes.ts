68c9c7368229470af76c5a1d6537f231e621c493
Timeuh <timeuuh@gmail.com>
2023-07-13 15:40:31 +0200

    feat: register user get by email route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,13 @@
 export {};
 
-const {createUser} = require('../controllers/user.controller');
+const {createUser, getUser} = require('../controllers/user.controller');
 const router = require('express').Router();
 
 // route to create a user
 router.post('/new', createUser);
+
+// route to get a user from an email
+router.get('/get', getUser);
 
 // export the router
 module.exports = router;
