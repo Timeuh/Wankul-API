e409170889c6cb7e0e4d01eb8a1a365db4d54a80
Timeuh <timeuuh@gmail.com>
2023-07-14 21:08:59 +0200

    feat: register rarity getter route

--- src/routes/backend/rarities.routes.ts
+++ src/routes/backend/rarities.routes.ts
@@ -1,10 +1,13 @@
 export{}
 
-const {createRarity} = require('../../controllers/backend/rarities.controller');
+const {createRarity, getRarity} = require('../../controllers/backend/rarities.controller');
 const router = require('express').Router();
 
 // route to create a rarity
 router.post('/new', createRarity);
+
+// route to get a rarity
+router.get('/get', getRarity);
 
 // export the router
 module.exports = router;
