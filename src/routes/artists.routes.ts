088a10263aec8a9b208e87e6cd50f5c4de566313
Timeuh <timeuuh@gmail.com>
2023-07-14 18:56:11 +0200

    feat: register artist get route

--- src/routes/artists.routes.ts
+++ src/routes/artists.routes.ts
@@ -1,8 +1,11 @@
-const {createArtist} = require("../controllers/artists.controller");
+const {createArtist, getArtist} = require("../controllers/artists.controller");
 const router = require('express').Router();
 
 // route to create an artist
 router.post('/new', createArtist);
+
+// route to get an artist
+router.get('/get', getArtist);
 
 // export the router
 module.exports = router;
