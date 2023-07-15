ca510aaf8595379a79141c14f6bf8258a8396d72
Timeuh <timeuuh@gmail.com>
2023-07-15 20:33:36 +0200

    feat: register character CRUD routes

---
+++ src/routes/backend/character.routes.ts
@@ -0,0 +1,19 @@
+export{}
+
+const {createCharacter, getCharacter, updateCharacter, deleteCharacter} = require('../../controllers/backend/character.controller');
+const router = require('express').Router();
+
+// route to create a character
+router.post('/new', createCharacter);
+
+// route to get a character
+router.get('/get', getCharacter);
+
+// route to update a character
+router.put('/update', updateCharacter);
+
+// route to delete a character
+router.delete('/delete', deleteCharacter);
+
+// export the router
+module.exports = router;
