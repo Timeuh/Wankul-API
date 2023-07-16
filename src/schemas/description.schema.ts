55ec607f402a670b8d36c50bcdafd9de76d12c27
Timeuh <timeuuh@gmail.com>
2023-07-16 10:18:30 +0200

    refactor: adapt schema to request parameters

--- src/schemas/description.schema.ts
+++ src/schemas/description.schema.ts
@@ -5,14 +5,14 @@ const zod = require('zod');
 
 // create description schema
 const DescriptionSchema = zod.object({
-  character_id: zod.preprocess((character_id: string) => parseInt(character_id), zod.number().positive()),
-  citation: zod.string(),
-  effect: zod.string(),
+  character_id: zod.preprocess((character_id: string) => parseInt(character_id), zod.number()),
+  citation: zod.string().optional(),
+  effect: zod.string().optional(),
   id: zod.preprocess((id: string) => parseInt(id), zod.number().positive()),
-  looser_effect: zod.string(),
+  looser_effect: zod.string().optional(),
   rarity_id: zod.preprocess((rarity_id: string) => parseInt(rarity_id), zod.number().positive()),
-  special: zod.string(),
-  winner_effect: zod.string()
+  special: zod.string().optional(),
+  winner_effect: zod.string().optional()
 });
 
 module.exports = DescriptionSchema;
