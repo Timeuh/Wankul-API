2ebe96d396f3ec1807c0c9abca666fb1a7bab009
Timeuh <timeuuh@gmail.com>
2023-07-16 10:21:16 +0200

    refactor: register description CRUD routes

---
+++ src/routes/backend/descriptions.routes.ts
@@ -0,0 +1,19 @@
+export{}
+
+const {createDescription, getDescription, updateDescription, deleteDescription} = require('../../controllers/backend/Descriptions.controller');
+const router = require('express').Router();
+
+// route to create a description
+router.post('/new', createDescription);
+
+// route to get a description
+router.get('/get', getDescription);
+
+// route to update a description
+router.put('/update', updateDescription);
+
+// route to delete a description
+router.delete('/delete', deleteDescription);
+
+// export the router
+module.exports = router;
