40ded71ddbd16168040a81c3c05dc4a7438a4f2c
Timeuh <timeuuh@gmail.com>
2023-07-16 23:02:53 +0200

    feat: create type for request type (api or backend) for prisma global get method

--- src/utils/api.types.ts
+++ src/utils/api.types.ts
@@ -1,1 +1,5 @@
+// list of database table names
 export type Model = 'artist' | 'card' | 'character' | 'description' | 'rarity' | 'type' | 'user';
+
+// types of request for prisma services
+export type RequestType = 'backend' | 'api';
