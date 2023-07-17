1ebb5acf9b170e5d0d53af65575c5b442d342b01
Timeuh <timeuuh@gmail.com>
2023-07-17 23:22:30 +0200

    feat: register api route to get all artists

--- src/routes/api/artists.routes.ts
+++ src/routes/api/artists.routes.ts
@@ -1,10 +1,13 @@
 export{}
 
-const {getArtistById} = require("../../controllers/api/artists.controller");
+const {getArtistById, getAllArtists} = require("../../controllers/api/artists.controller");
 const router = require('express').Router();
 
 // route to create an artist
 router.get('/:id', getArtistById);
+
+// route to get all artists
+router.get('/', getAllArtists);
 
 // export the router
 module.exports = router;
