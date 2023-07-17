ef3a8e08bb8b6e0ef9e8479ac649d0dccaefc9c1
Timeuh <timeuuh@gmail.com>
2023-07-18 00:05:33 +0200

    feat: create cards getter by artist method (WIP)

--- src/controllers/api/artists.controller.ts
+++ src/controllers/api/artists.controller.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from 'express';
 
 const {getEntity} = require('../../utils/prisma/get-entity');
+const {getEntityCards} = require('../../utils/prisma/get-entity-cards');
 const {getAllEntities} = require('../../utils/prisma/get-all-entities');
 const {fillResponseForAllEntities} = require('../../utils/api.functions');
 const artistSchema = require('../../schemas/artist.schema');
@@ -39,6 +40,23 @@ module.exports.getAllArtists = async (request: Request, response: Response) => {
 
     // return the artists
     return response.status(200).json(responseData);
+  } catch (error: any){
+    // in case of error, return the error
+    return response.status(500).json({
+      code: 500,
+      message: error.message
+    });
+  }
+}
+
+// get artist related cards
+module.exports.getArtistCards = async (request: Request, response: Response) => {
+  try {
+    // get artist cards
+    const artistCards = await getEntityCards(request, 'artist');
+
+    // return the artists
+    return response.status(200).json(artistCards);
   } catch (error: any){
     // in case of error, return the error
     return response.status(500).json({
