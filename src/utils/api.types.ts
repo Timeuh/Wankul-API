d0ff2bb5fd8240ad00419babf639cf27f26ebb03
Timeuh <timeuuh@gmail.com>
2023-07-17 23:20:21 +0200

    feat: create type for database tables plural names

--- src/utils/api.types.ts
+++ src/utils/api.types.ts
@@ -1,5 +1,8 @@
 // list of database table names
 export type Model = 'artist' | 'card' | 'character' | 'description' | 'rarity' | 'type' | 'user';
 
+// plural names for models
+export type PluralModel = 'artists' | 'cards' | 'characters' | 'descriptions' | 'rarities' | 'types' | 'users';
+
 // types of request for prisma services
 export type RequestType = 'backend' | 'api';
