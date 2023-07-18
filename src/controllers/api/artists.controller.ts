0e34b08404283773acf55f21f8ab9d908645ca21
Timeuh <timeuuh@gmail.com>
2023-07-18 20:05:27 +0200

    feat: complete getArtistCards function to return an object with artists infos and a list of cards

--- src/controllers/api/artists.controller.ts
+++ src/controllers/api/artists.controller.ts
@@ -1,9 +1,10 @@
 import {Request, Response} from 'express';
+import {Card, CardsResponse} from '../../utils/api.types';
 
 const {getEntity} = require('../../utils/prisma/get-entity');
 const {getEntityCards} = require('../../utils/prisma/get-entity-cards');
 const {getAllEntities} = require('../../utils/prisma/get-all-entities');
-const {fillResponseForAllEntities} = require('../../utils/api.functions');
+const {fillResponseForAllEntities, fillResponseWithCards} = require('../../utils/api.functions');
 const artistSchema = require('../../schemas/artist.schema');
 
 // get an artist from an id
@@ -55,8 +56,25 @@ module.exports.getArtistCards = async (request: Request, response: Response) => 
     // get artist cards
     const artistCards = await getEntityCards(request, 'artist');
 
+    // create response object
+    const responseData: CardsResponse<'artist'> = {
+      artist: artistCards[0].artist,
+      links: {
+        self: `/api/artist/${artistCards[0].artist.id}`,
+        all: '/api/artist/'
+      },
+      cards:{
+        type: 'collection',
+        length: artistCards.length,
+        cards: [] as Card[]
+      }
+    }
+
+    // convert Cards to ActualCards
+    fillResponseWithCards(responseData, artistCards);
+
     // return the artists
-    return response.status(200).json(artistCards);
+    return response.status(200).json(responseData);
   } catch (error: any){
     // in case of error, return the error
     return response.status(500).json({
