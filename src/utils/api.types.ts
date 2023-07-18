912e4ecb029d6c90eef67958c9755f85bd3cc5c2
Timeuh <timeuuh@gmail.com>
2023-07-18 20:03:30 +0200

    feat: create 2 new types : one for a complete Card, and one for an api response containing a cards array

--- src/utils/api.types.ts
+++ src/utils/api.types.ts
@@ -6,3 +6,54 @@ export type PluralModel = 'artists' | 'cards' | 'characters' | 'descriptions' | 
 
 // types of request for prisma services
 export type RequestType = 'backend' | 'api';
+
+export type CardsResponse<T extends Model> = {
+  [Key in T]: object
+} & {
+  links: {
+    self: string,
+    all: string,
+  },
+  cards: {
+    type: string,
+    length: number,
+    cards: Card[]
+  },
+}
+
+// represent a card with all attributes
+export type Card = {
+  id: number,
+  name: string,
+  collection: string,
+  description_id?: number,
+  type_id?: number,
+  artist_id?: number,
+  image: string,
+  artist: {
+    id: number,
+    name: string
+  },
+  description: {
+    id: number,
+    winner_effect: string,
+    looser_effect: string,
+    special: string,
+    character_id?: number,
+    effect: string,
+    citation: string,
+    rarity_id?: number,
+    character: {
+      id: number,
+      name: string
+    },
+    rarity: {
+      id: number,
+      name: string
+    }
+  },
+  type: {
+    id: number,
+    name: string
+  }
+}
