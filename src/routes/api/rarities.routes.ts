8721430a3a4555a9b592b0e79ea1312871b29260
Timeuh <timeuuh@gmail.com>
2023-07-18 21:20:51 +0200

    feat: register api routes for rarities : get by id, get all and get cards

---
+++ src/routes/api/rarities.routes.ts
@@ -0,0 +1,16 @@
+export{}
+
+const {getRarityById, getAllRarities, getRarityCards} = require("../../controllers/api/rarities.controller");
+const router = require('express').Router();
+
+// route to create a rarity
+router.get('/:id', getRarityById);
+
+// route to get all rarities
+router.get('/', getAllRarities);
+
+// route to get all rarities
+router.get('/:id/cards', getRarityCards);
+
+// export the router
+module.exports = router;
